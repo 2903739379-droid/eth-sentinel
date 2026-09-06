6ab9fef786b3449f563d600bfd7a1aa3d8b3a345
2903739379-droid <2903739379@qq.com>
2026-09-06 23:26:54 +0800

    升级极速缓存并后台更新页面

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='eth-sentinel-v1';
-const CORE=['./','./index.html','./sw.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;if(r.mode==='navigate'){e.respondWith(caches.match('./index.html').then(cached=>{const net=fetch(r).then(res=>{if(res.ok)caches.open(CACHE).then(c=>c.put('./index.html',res.clone()));return res}).catch(()=>cached);return cached||net}))}else if(new URL(r.url).origin===location.origin){e.respondWith(caches.match(r).then(c=>c||fetch(r)))}});
+const CACHE='eth-sentinel-v2';
+const CORE=['./','./index.html','./sw.js','./news-translate.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;if(r.mode==='navigate'){e.respondWith(caches.match('./index.html').then(cached=>{const net=fetch(r).then(res=>{if(res.ok)caches.open(CACHE).then(c=>c.put('./index.html',res.clone()));return res}).catch(()=>cached);return cached||net}))}else if(new URL(r.url).origin===location.origin)e.respondWith(caches.match(r).then(c=>c||fetch(r))) });
